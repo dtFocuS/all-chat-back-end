a4cb26731ab8e32367a5ffb5478aecdcb23e86e5
Danny <dtfocus1025@gmail.com>
2020-03-17 22:23:27 -0700

    configuring action cable

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::RoomsController < ApplicationController
+    skip_before_action :authorized
+
+    def index
+        rooms = Room.all
+        render :json => rooms, status: :accepted
+    end
+
+
+
+    
 end
