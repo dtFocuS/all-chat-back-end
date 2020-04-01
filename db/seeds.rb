eba004b1ae7873847e2d77f25abda23fd1325331
Danny <dtfocus1025@gmail.com>
2020-03-31 23:00:04 -0700

    adjusting seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Friendship.destroy_all;
-Room.destroy_all;
-Message.destroy_all;
+# Friendship.destroy_all;
+# Room.destroy_all;
+# Message.destroy_all;
 # User.destroy_all;
